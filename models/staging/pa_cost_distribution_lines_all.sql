149edba9ba139aa975b1ef400f05fbbf70bfb52d
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-09-26 15:44:12 +0530

    Add/Update model for pa_cost_distribution_lines_all

---
+++ models/staging/pa_cost_distribution_lines_all.sql
@@ -0,0 +1,13 @@
+{{{{ config(
+  materialized='incremental',
+  unique_key='id'
+) }}}}
+
+select *
+from { source('redshift_src', 'pa_cost_distribution_lines_all') }
+{{% if is_incremental() %}}
+  where last_update_date >= (
+    select coalesce(max(last_update_date), to_timestamp('1900-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS'))
+    from {{ this }}
+  )
+{{% endif %}}
