b1b0ba104497fefeb1f5815ccfcaccc5db36966f
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-09-26 16:15:23 +0530

    Add/Update model for fnd_flex_values_vl_tmp

---
+++ models/prestage_1/fnd_flex_values_vl_tmp.sql
@@ -0,0 +1,6 @@
+{{{{ config(materialized="incremental", unique_key="integration_id") }}}}
+
+select
+  *,
+  flex_value_id as integration_id
+from {{ source("redshift_src", "fnd_flex_values_vl_tmp") }}
