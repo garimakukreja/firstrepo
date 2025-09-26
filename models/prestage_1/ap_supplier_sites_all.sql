05d00561d72739b575f74737dbe6d76a33d5c286
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-09-26 16:13:40 +0530

    Add/Update model for ap_supplier_sites_all

---
+++ models/prestage_1/ap_supplier_sites_all.sql
@@ -0,0 +1,6 @@
+{{{{ config(materialized="incremental", unique_key="integration_id") }}}}
+
+select
+  *,
+  vendor_site_id as integration_id
+from {{ source("redshift_src", "ap_supplier_sites_all") }}
