54ebde5a78aee7deca1e39db4e828c3fd3115f2a
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-09-26 15:40:27 +0530

    Add/Update model for ar_receipt_methods_tmp

---
+++ models/staging/ar_receipt_methods_tmp.sql
@@ -0,0 +1,13 @@
+{{{{ config(
+  materialized='incremental',
+  unique_key='receipt_method_id'
+) }}}}
+
+select *
+from { source('redshift_src', 'ar_receipt_methods_tmp') }
+{{% if is_incremental() %}}
+  where last_update_date >= (
+    select coalesce(max(last_update_date), to_timestamp('1900-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS'))
+    from {{ this }}
+  )
+{{% endif %}}
