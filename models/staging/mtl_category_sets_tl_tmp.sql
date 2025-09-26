36b90e979e9ab7f06ffa44bfa1868690474360b6
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-09-26 15:43:25 +0530

    Add/Update model for mtl_category_sets_tl_tmp

---
+++ models/staging/mtl_category_sets_tl_tmp.sql
@@ -0,0 +1,13 @@
+{{{{ config(
+  materialized='incremental',
+  unique_key='id'
+) }}}}
+
+select *
+from { source('redshift_src', 'mtl_category_sets_tl_tmp') }
+{{% if is_incremental() %}}
+  where last_update_date >= (
+    select coalesce(max(last_update_date), to_timestamp('1900-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS'))
+    from {{ this }}
+  )
+{{% endif %}}
