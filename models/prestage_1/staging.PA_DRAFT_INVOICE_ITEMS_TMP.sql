f039aa0fc5547783bf1f2fe7f8f36d70883f69cb
garima.kukreja@appsassociates.com <garima.kukreja@appsassociates.com>
2025-09-26 08:48:22 +0000

    na

--- models/prestage_1/staging.PA_DRAFT_INVOICE_ITEMS_TMP.sql
+++
@@ -1,8 +0,0 @@
-select *
-from `dev.staging.staging.PA_DRAFT_INVOICE_ITEMS_TMP`
-{% if is_incremental() %}
-  where LASTUPDATEDATE >= (
-    select coalesce(max(LASTUPDATEDATE), timestamp('1900-01-01 00:00:00'))
-    from {{ this }}
-  )
-{% endif %}
