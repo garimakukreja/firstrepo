65d13738247fd16e0f3668c47db105a6fc40a153
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-09-26 14:11:40 +0530

    Add/Update model for staging.MTL_ONHAND_QUANTITIES_DETAIL

---
+++ models/prestage_1/staging.MTL_ONHAND_QUANTITIES_DETAIL.sql
@@ -0,0 +1,8 @@
+select *
+from `dev.staging.staging.MTL_ONHAND_QUANTITIES_DETAIL`
+{% if is_incremental() %}
+  where LASTUPDATEDATE >= (
+    select coalesce(max(LASTUPDATEDATE), timestamp('1900-01-01 00:00:00'))
+    from {{ this }}
+  )
+{% endif %}
