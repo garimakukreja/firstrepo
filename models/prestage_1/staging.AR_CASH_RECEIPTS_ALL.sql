543dd5ab9390ce9e8269c389f4f21aa0a8182f63
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-09-26 14:08:29 +0530

    Add/Update model for staging.AR_CASH_RECEIPTS_ALL

---
+++ models/prestage_1/staging.AR_CASH_RECEIPTS_ALL.sql
@@ -0,0 +1,8 @@
+select *
+from `dev.staging.staging.AR_CASH_RECEIPTS_ALL`
+{% if is_incremental() %}
+  where LASTUPDATEDATE >= (
+    select coalesce(max(LASTUPDATEDATE), timestamp('1900-01-01 00:00:00'))
+    from {{ this }}
+  )
+{% endif %}
