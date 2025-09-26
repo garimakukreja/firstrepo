80658a9f3231872efe9c5812141fd47bc17c3f5e
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-09-26 16:19:23 +0530

    Add/Update model for po_distributions_all_tmp

---
+++ models/prestage_1/po_distributions_all_tmp.sql
@@ -0,0 +1,6 @@
+{{{{ config(materialized="incremental", unique_key="integration_id") }}}}
+
+select
+  *,
+  po_distribution_id as integration_id
+from {{ source("redshift_src", "po_distributions_all_tmp") }}
