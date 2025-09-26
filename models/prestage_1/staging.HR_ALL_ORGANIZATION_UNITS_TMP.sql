5df086a9da39fbb74dcbbecd6dc78f48e9286785
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-09-26 14:10:45 +0530

    Add/Update model for staging.HR_ALL_ORGANIZATION_UNITS_TMP

---
+++ models/prestage_1/staging.HR_ALL_ORGANIZATION_UNITS_TMP.sql
@@ -0,0 +1,8 @@
+select *
+from `dev.staging.staging.HR_ALL_ORGANIZATION_UNITS_TMP`
+{% if is_incremental() %}
+  where LASTUPDATEDATE >= (
+    select coalesce(max(LASTUPDATEDATE), timestamp('1900-01-01 00:00:00'))
+    from {{ this }}
+  )
+{% endif %}
