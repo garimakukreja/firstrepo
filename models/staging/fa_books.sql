741a4c7c5fdc407f1875384b1caf54a16d234263
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-09-26 15:41:06 +0530

    Add/Update model for fa_books

---
+++ models/staging/fa_books.sql
@@ -0,0 +1,13 @@
+{{{{ config(
+  materialized='incremental',
+  unique_key='id'
+) }}}}
+
+select *
+from { source('redshift_src', 'fa_books') }
+{{% if is_incremental() %}}
+  where last_update_date >= (
+    select coalesce(max(last_update_date), to_timestamp('1900-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS'))
+    from {{ this }}
+  )
+{{% endif %}}
