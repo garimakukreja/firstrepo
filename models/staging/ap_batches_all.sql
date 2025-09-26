3f6a9ef386f8060137d70f159642ad561a48c853
garima.kukreja@appsassociates.com <garima.kukreja@appsassociates.com>
2025-09-26 09:10:23 +0000

    na

--- models/staging/ap_batches_all.sql
+++ models/staging/ap_batches_all.sql
@@ -1,4 +1,4 @@
 {{ config(materialized="incremental", unique_key="batch_id") }}
 
 select *
-from {{ sources("redshift_src", "ap_batches_all") }}
+from {{ source("redshift_src", "ap_batches_all") }}
