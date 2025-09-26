7e365fbbe6a53b8baf1781d79b71503f24b58fd1
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-09-26 16:20:28 +0530

    Add/Update model for xle_entity_profiles

---
+++ models/prestage_1/xle_entity_profiles.sql
@@ -0,0 +1,6 @@
+{{{{ config(materialized="incremental", unique_key="integration_id") }}}}
+
+select
+  *,
+  legal_entity_id as integration_id
+from {{ source("redshift_src", "xle_entity_profiles") }}
