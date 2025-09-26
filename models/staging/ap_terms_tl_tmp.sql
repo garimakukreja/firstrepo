9df1f84c28cbb2cd0b034d5b043a8ad5b27be09a
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-09-26 15:40:11 +0530

    Add/Update model for ap_terms_tl_tmp

---
+++ models/staging/ap_terms_tl_tmp.sql
@@ -0,0 +1,13 @@
+{{{{ config(
+  materialized='incremental',
+  unique_key=['term_id','language']
+) }}}}
+
+select *
+from { source('redshift_src', 'ap_terms_tl_tmp') }
+{{% if is_incremental() %}}
+  where last_update_date >= (
+    select coalesce(max(last_update_date), to_timestamp('1900-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS'))
+    from {{ this }}
+  )
+{{% endif %}}
