d775e4b553dd0f53c0c44f3169fac94333ce0c93
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-09-26 14:08:43 +0530

    Add/Update model for staging.CE_BANK_ACCOUNTS_TMP

---
+++ models/prestage_1/staging.CE_BANK_ACCOUNTS_TMP.sql
@@ -0,0 +1,8 @@
+select *
+from `dev.staging.staging.CE_BANK_ACCOUNTS_TMP`
+{% if is_incremental() %}
+  where LASTUPDATEDATE >= (
+    select coalesce(max(LASTUPDATEDATE), timestamp('1900-01-01 00:00:00'))
+    from {{ this }}
+  )
+{% endif %}
