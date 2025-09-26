2412f0b5ef81633a96204605a0a085b8efbbb517
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-09-26 14:10:01 +0530

    Add/Update model for staging.FND_FLEX_VALUE_SETS_TMP

---
+++ models/prestage_1/staging.FND_FLEX_VALUE_SETS_TMP.sql
@@ -0,0 +1,8 @@
+select *
+from `dev.staging.staging.FND_FLEX_VALUE_SETS_TMP`
+{% if is_incremental() %}
+  where LASTUPDATEDATE >= (
+    select coalesce(max(LASTUPDATEDATE), timestamp('1900-01-01 00:00:00'))
+    from {{ this }}
+  )
+{% endif %}
