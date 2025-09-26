1f4d3fb5d6fbd03ef1e649ad3199ab18f23c8bbf
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-09-26 16:13:25 +0530

    Add/Update model for ap_invoice_lines_all_tmp

---
+++ models/prestage_1/ap_invoice_lines_all_tmp.sql
@@ -0,0 +1,6 @@
+{{{{ config(materialized="incremental", unique_key="integration_id") }}}}
+
+select
+  *,
+  invoice_line_id as integration_id
+from {{ source("redshift_src", "ap_invoice_lines_all_tmp") }}
