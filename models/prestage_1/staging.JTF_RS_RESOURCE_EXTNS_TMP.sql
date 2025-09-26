2820363c1a8dca73e03b4267390ff2afa32ec298
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-09-26 14:11:19 +0530

    Add/Update model for staging.JTF_RS_RESOURCE_EXTNS_TMP

---
+++ models/prestage_1/staging.JTF_RS_RESOURCE_EXTNS_TMP.sql
@@ -0,0 +1,8 @@
+select *
+from `dev.staging.staging.JTF_RS_RESOURCE_EXTNS_TMP`
+{% if is_incremental() %}
+  where LASTUPDATEDATE >= (
+    select coalesce(max(LASTUPDATEDATE), timestamp('1900-01-01 00:00:00'))
+    from {{ this }}
+  )
+{% endif %}
