1dfa98cc14b74c24d172eb5b57a5d5482ba106f4
garima.kukreja@appsassociates.com <garima.kukreja@appsassociates.com>
2025-09-26 08:49:32 +0000

    na

--- models/staging/stage_products.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='incremental') }}
-
-select
-    {{ oracle_nextval('staging', 'products_seq') }} as product_sk,
-    product_id,
-    product_name,
-    product_price
-from {{ source('raw', 'products') }}
