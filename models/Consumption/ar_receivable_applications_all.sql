92f660674adb4900bc549e0e90c47ec03017b65c
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-09-29 13:57:43 +0530

    Add/Update model for ar_receivable_applications_all

---
+++ models/Consumption/ar_receivable_applications_all.sql
@@ -0,0 +1,6 @@
+{{ config(materialized="view", unique_key="integration_id") }}
+
+select
+  *,
+  receivable_application_id as integration_id
+from {{ source("redshift_src", "ar_receivable_applications_all") }}
