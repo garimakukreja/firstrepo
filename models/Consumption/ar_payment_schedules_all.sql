4c85fd2d4ffa5dd202264ef90bc53b46b48fab6c
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-09-29 13:57:51 +0530

    Add/Update model for ar_payment_schedules_all

---
+++ models/Consumption/ar_payment_schedules_all.sql
@@ -0,0 +1,6 @@
+{{ config(materialized="view", unique_key="integration_id") }}
+
+select
+  *,
+  payment_schedule_id as integration_id
+from {{ source("redshift_src", "ar_payment_schedules_all") }}
