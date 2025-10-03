4d21ff57da4e7b39bc73923d315a12e0298e8173
garima.kukreja@appsassociates.com <garima.kukreja@appsassociates.com>
2025-10-03 14:59:54 +0000

    na

--- models/Consumption/hr_all_organization_units_tl.sql
+++ models/Consumption/hr_all_organization_units_tl.sql
@@ -1,6 +1,5 @@
-{{ config(materialized="view", unique_key="integration_id") }}
+{{ config(materialized="view") }}
 
 select
-  *,
-  ["ORGANIZATION_ID", "LANGUAGE"] as integration_id
+  *
 from {{ source("redshift_src", "hr_all_organization_units_tl") }}
