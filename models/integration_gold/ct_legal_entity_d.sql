273f887d545889745d462609e700130c360e9e84
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-10-07 15:07:37 +0530

    Promote dbt model to integration_gold: ct_legal_entity_d.sql (2025-10-07 15:07:37)

---
+++ models/integration_gold/ct_legal_entity_d.sql
@@ -0,0 +1,31 @@
+{{ 
+    config(
+        materialized = "incremental",
+        unique_key = "integration_id",
+        post_hook = "{{ mkInsert_unspecified_row(this.name) }}"
+    ) 
+}}
+
+with
+    ct_legal_entity_d_stg as (
+        {% if is_incremental() %}
+            select stg.*, coalesce(tgt.w_insert_dt, current_timestamp()) as w_insert_dt
+            from {{ ref("CT_LEGAL_ENTITY_D_STG") }} stg
+            left outer join {{ this }} tgt
+                on stg.integration_id = tgt.integration_id
+        {% else %}
+            select stg.*, current_timestamp() as w_insert_dt
+            from {{ ref("CT_LEGAL_ENTITY_D_STG") }} stg
+        {% endif %}
+    ),
+
+    ct_legal_entity_d as (
+        select
+            {{ dbt_utils.surrogate_key(['integration_id']) }} as bu_key,
+            *,
+            current_timestamp() as w_update_dt
+        from ct_legal_entity_d_stg
+    )
+
+select *
+from ct_legal_entity_d;
