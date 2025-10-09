5911014cc176c03c370dc60197ee006571df3208
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-10-09 13:16:51 +0530

    Add/Update model for HR_OPERATING_UNITS

--- models/Consumption/hr_operating_units.sql
+++ models/Consumption/hr_operating_units.sql
@@ -1,5 +1,6 @@
-{{ config(materialized="view", unique_key="integration_id") }}
+{{ config(materialized="incremental", unique_key=["hr_operating_units_key"]) }}
 
 select
-  *
+  *,
+  organization_id as hr_operating_units_key
 from {{ source("redshift_src", "hr_operating_units") }}
