c79581377c80fccff5cc005552e8f4d3df58ad96
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-10-09 13:16:37 +0530

    Add/Update model for XLE_REGISTRATIONS

--- models/Consumption/xle_registrations.sql
+++ models/Consumption/xle_registrations.sql
@@ -1,5 +1,6 @@
-{{ config(materialized="view", unique_key="integration_id") }}
+{{ config(materialized="incremental", unique_key=["xle_registrations_key"]) }}
 
 select
-  *
+  *,
+  registration_id as xle_registrations_key
 from {{ source("redshift_src", "xle_registrations") }}
