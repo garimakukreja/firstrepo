861c928785585da2a03d678b45d286d05cf59e68
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-10-09 13:16:33 +0530

    Add/Update model for FND_ID_FLEX_SEGMENTS

--- models/Consumption/fnd_id_flex_segments.sql
+++ models/Consumption/fnd_id_flex_segments.sql
@@ -1,9 +1,6 @@
-{{ config(materialized="view") }}
+{{ config(materialized="incremental", unique_key=["fnd_id_flex_segments_key"]) }}
 
 select
   *,
-  CAST(application_id AS VARCHAR) || '~' ||
-  id_flex_code || '~' ||
-  CAST(id_flex_num AS VARCHAR) || '~' ||
-  application_column_name AS integration_id
+  application_id||'~'||id_flex_code||'~'||id_flex_num||'~'||application_column_name as fnd_id_flex_segments_key
 from {{ source("redshift_src", "fnd_id_flex_segments") }}
