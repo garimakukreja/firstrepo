76f351dd0670b26b39a83a0048b863d968d4a301
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-10-09 13:16:41 +0530

    Add/Update model for ORG_ORGANIZATION_DEFINITIONS

--- models/Consumption/org_organization_definitions.sql
+++ models/Consumption/org_organization_definitions.sql
@@ -1,5 +1,6 @@
-{{ config(materialized="view", unique_key="integration_id") }}
+{{ config(materialized="incremental", unique_key=["org_organization_definitions_key"]) }}
 
 select
-  *
+  *,
+  organization_id as org_organization_definitions_key
 from {{ source("redshift_src", "org_organization_definitions") }}
