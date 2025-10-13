9079af03e8371a4f4c22f70a0c425b3b891869b2
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-10-13 14:35:43 +0530

    Add/Update model for mtl_system_items_b

--- models/Consumption/material_system_item.sql
+++ models/Consumption/material_system_item.sql
@@ -1,6 +1,6 @@
-{{ config(materialized="incremental", unique_key=["material_system_item_key"]) }}
+{{ config(materialized="incremental", unique_key="integration_id") }}
 
 select
   *,
-  inventory_item_id||'~'||organization_id as material_system_item_key
-from {{ source("redshift_src", "mtl_system_items_b") }}
+  inventory_item_id||'~'||organization_id as integration_id
+from {{ source("ebs", "mtl_system_items_b") }}
