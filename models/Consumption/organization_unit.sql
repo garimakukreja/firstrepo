242e3008be39f8f0c9fb1f015fad48fd547aab49
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-10-13 14:35:51 +0530

    Add/Update model for hr_all_organization_units

--- models/Consumption/organization_unit.sql
+++ models/Consumption/organization_unit.sql
@@ -1,6 +1,6 @@
-{{ config(materialized="incremental", unique_key=["organization_unit_key"]) }}
+{{ config(materialized="incremental", unique_key="integration_id") }}
 
 select
   *,
-  organization_id as organization_unit_key
-from {{ source("redshift_src", "hr_all_organization_units") }}
+  organization_id as integration_id
+from {{ source("ebs", "hr_all_organization_units") }}
