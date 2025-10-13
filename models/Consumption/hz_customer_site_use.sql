7547b9a8cd675c370bb050f8c4d3bdfef6876327
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-10-13 14:36:21 +0530

    Add/Update model for hz_cust_site_uses_all

--- models/Consumption/hz_customer_site_use.sql
+++ models/Consumption/hz_customer_site_use.sql
@@ -1,6 +1,6 @@
-{{ config(materialized="incremental", unique_key=["hz_customer_site_use_key"]) }}
+{{ config(materialized="incremental", unique_key="integration_id") }}
 
 select
   *,
-  site_use_id as hz_customer_site_use_key
-from {{ source("redshift_src", "hz_cust_site_uses_all") }}
+  site_use_id as integration_id
+from {{ source("ebs", "hz_cust_site_uses_all") }}
