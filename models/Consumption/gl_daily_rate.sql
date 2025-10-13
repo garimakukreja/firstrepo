2c187403d809b213b54aeb74a0bf0bbd132a8472
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-10-13 14:35:47 +0530

    Add/Update model for gl_daily_rates

--- models/Consumption/gl_daily_rate.sql
+++ models/Consumption/gl_daily_rate.sql
@@ -1,6 +1,6 @@
-{{ config(materialized="incremental", unique_key=["gl_daily_rate_key"]) }}
+{{ config(materialized="incremental", unique_key="integration_id") }}
 
 select
   *,
-  from_currency||'~'||to_currency||'~'||conversion_date||'~'||conversion_type as gl_daily_rate_key
-from {{ source("redshift_src", "gl_daily_rates") }}
+  from_currency||'~'||to_currency||'~'||conversion_date||'~'||conversion_type as integration_id
+from {{ source("ebs", "gl_daily_rates") }}
