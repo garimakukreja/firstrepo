7a158369f492086886e0038be563e3a8fbfc0a5e
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-10-13 14:36:08 +0530

    Add/Update model for ra_customer_trx_lines_all

--- models/Consumption/ra_customer_trx_line.sql
+++ models/Consumption/ra_customer_trx_line.sql
@@ -1,6 +1,6 @@
-{{ config(materialized="incremental", unique_key=["ra_customer_trx_line_key"]) }}
+{{ config(materialized="incremental", unique_key="integration_id") }}
 
 select
   *,
-  customer_trx_line_id as ra_customer_trx_line_key
-from {{ source("redshift_src", "ra_customer_trx_lines_all") }}
+  customer_trx_line_id as integration_id
+from {{ source("ebs", "ra_customer_trx_lines_all") }}
