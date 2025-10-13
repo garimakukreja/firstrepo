f8c7ff2e7ae9590eb5ae77fdc9544e19d431dbe7
garimakukreja <55616347+garimakukreja@users.noreply.github.com>
2025-10-13 14:35:49 +0530

    Add/Update model for mtl_parameters

--- models/Consumption/material_parameter.sql
+++ models/Consumption/material_parameter.sql
@@ -1,6 +1,6 @@
-{{ config(materialized="incremental", unique_key=["material_parameter_key"]) }}
+{{ config(materialized="incremental", unique_key="integration_id") }}
 
 select
   *,
-  organization_id as material_parameter_key
-from {{ source("redshift_src", "mtl_parameters") }}
+  organization_id as integration_id
+from {{ source("ebs", "mtl_parameters") }}
