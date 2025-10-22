6a2256020a6f57e20e6da9bb7a11133f0676d29e
Garima Kukreja <garima.kukreja@appsassociates.com>
2025-10-22 07:31:25 +0000

    na

--- models/Consumption/ap_batches_all.sql
+++ models/Consumption/ap_batches_all.sql
@@ -1,11 +1,12 @@
-{{ config(
-    materialized="incremental",
-    unique_key="integration_id",
-    incremental_strategy="merge"
-) }}
+{{
+    config(
+        materialized="incremental",
+        unique_key="integration_id",
+        incremental_strategy="merge",
+    )
+}}
 
-select
-      *,
-    batch_id as integration_id
-from {{ source("ebs", "ap_batches_all") }}
-{{ incremental_filter_condition('last_update_date') }}
+SELECT *, batch_id as integration_id
+from
+    {{ source("ebs", "ap_batches_all") }}
+    {{ incremental_filter_condition("last_update_date") }}
